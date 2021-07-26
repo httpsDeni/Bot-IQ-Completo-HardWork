5485e589277de56ddf3b8d8559e7ea95d4fd36f6
Deni Trepak <87042731+httpsDeni@users.noreply.github.com>
2021-07-25 23:30:20 -0300

    Update models.HorarioOperacao.py

--- models.HorarioOperacao.py
+++ models.HorarioOperacao.py
@@ -1,9 +1,3 @@
-# decompyle3 version 3.3.2
-# Python bytecode 3.7 (3394)
-# Decompiled from: Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)]
-# Embedded file name: models\HorarioOperacao.py
-# Compiled at: 1995-09-27 13:18:56
-# Size of source mod 2**32: 272 bytes
 import time
 from datetime import datetime, timedelta
 
